d1ccc998edbf2a81d29da18e12a9c8ae4638e93f
yadeorsl <yadeorsl@gmail.com>
2024-03-09 13:54:49 +0300

    init project

---
+++ src/main/java/com/lab/labschooldemo/LabSchoolDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lab.labschooldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSchoolDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSchoolDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/labschooldemo/LabSchoolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.labschooldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSchoolDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
